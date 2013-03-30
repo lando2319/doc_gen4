ea1b3341a841ccd05ce593226071ccd8ebdd7100
Mike Land <mikepland@gmail.com>
2013-03-29 20:19:17 -0500

    nothing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 DocGen4::Application.routes.draw do
+
+  get 'homepage/index' => 'Homepage#index', :as => 'homepage'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
