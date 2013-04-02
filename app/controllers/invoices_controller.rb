c9cd77701840023350bbba9fa027768d7d03e388
Mike Land <mikepland@gmail.com>
2013-04-02 14:45:02 -0500

    fixed invoice form so it now takes data

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -4,15 +4,20 @@ class InvoicesController < ApplicationController
 
   def new
     @invoice = Invoice.new
+
+
+
   end
 
   def create
-    @invoice = Invoice.new(params[:Invoice])
-
-
-
+    @invoice = Invoice.new(params[:invoice])
+        respond_to do |format|
+      if @invoice.save
+        format.html { redirect_to root_url }
+      else
+        format.html { render action: "new" }
+    end
   end
-
-  belongs_to :person
+  end
 
 end
--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,6 +3,7 @@ class PeopleController < ApplicationController
   def new
     @person = Person.new
 
+
   end
 
   def create
